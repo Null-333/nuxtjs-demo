716ba1cc04eda28594a0a88942798983752a27e1
Helen <yinhelen.hlj@qq.com>
2022-09-06 17:35:19 +0800

    路由导航

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
     About Page!
+    <h2>a链接</h2>
+    <a href="/">首页</a>
+    <h2>router-link</h2>
+    <router-link to="/">首页</router-link>
+    <h2>编程导航</h2>
+    <button @click="onClick">首页</button>
   </div>
 </template>
 <script>
 export default {
-  name: 'AboutPage'
+  name: 'AboutPage',
+  methods: {
+    onClick() {
+      this.$router.push('/');
+    }
+  },
 }
 </script>
