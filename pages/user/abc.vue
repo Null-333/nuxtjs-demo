a30ebc756d3907cac33305461174a61bc22d838f
Helen <yinhelen.hlj@qq.com>
2022-09-06 16:00:16 +0800

    router

---
+++ pages/user/abc.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    abc3333
+  </div>
+</template>
+<script>
+export default {
+  name: 'Abc',
+}
+</script>
---
+++ pages/user/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    user Page
+  </div>
+</template>
+<script>
+export default {
+  name: 'UserPage',
+}
+</script>
